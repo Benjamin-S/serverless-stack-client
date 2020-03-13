8d93ad89af1411eb26aa338e42eab97bf5551e24
Benjamin Symons <benjaminsymons@hotmail.com>
2020-03-13 11:45:08 +0800

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: "notes-app-uploads-serverless"
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "https://pl81otylq0.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_apvXhBV4Y",
+        APP_CLIENT_ID: "4ibaoekq5v8c00ffeua95qsgtr",
+        IDENTITY_POOL_ID: "us-east-1:839cf399-125d-4642-8dfd-bbe2a45c963b"
+    }
+};
